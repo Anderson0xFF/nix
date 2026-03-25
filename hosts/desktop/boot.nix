070d49298b655281634dcfb53adde8ccb929721a
Anderson Almeida <Anderson Almeida>
2026-03-25 10:35:19 -0300

    update

--- hosts/desktop/boot.nix
+++ hosts/desktop/boot.nix
@@ -1,6 +1,14 @@
 { ... }:
 {
-  boot.loader.systemd-boot.enable = true;
+  # GRUB com EFI (tela bonita com logo do NixOS)
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.grub.configurationLimit = 10;
+
+  # systemd-boot (minimalista, sem tema)
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.configurationLimit = 10;
 }
--- modules/home/niri.nix
+++ modules/home/niri.nix
@@ -18,20 +18,20 @@ in
 
     layout = {
       gaps = 8;
-      struts = {
-        left = 64;
-        right = 64;
-      };
+      # struts = {
+      #   left = 64;
+      #   right = 64;
+      # };
 
       # Configuração recomendada para workflow de código
       always-center-single-column = true;  # Editor centralizado quando sozinho
-      # center-focused-column = "on-overflow";  # Centraliza se não couber
+      center-focused-column = "always";
       # preset-column-widths = [
       #   { proportion = 0.33333; }  # Terminal lateral
       #   { proportion = 0.5; }      # Editor balanceado
       #   { proportion = 0.66667; }  # Browser principal
       # ];
-      default-column-width = { proportion = 0.9; };  # Padrão 50%
+      default-column-width = { proportion = 0.5; };  # Padrão 50%
 
       border = {
         enable = true;
