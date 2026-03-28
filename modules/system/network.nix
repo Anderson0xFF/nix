02d7e83c3418fac7a8119e75b67eeb122457aa5a
Anderson Almeida <Anderson Almeida>
2026-03-28 20:50:16 -0300

    refatora configuração do NetworkManager e corrige formatação dos nameservers

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -3,12 +3,15 @@
   networking = {
     # hostName é definido em cada host (hosts/*/default.nix)
     # hostName = "nixos-workstation";
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      dns = "none";
+    };
     enableIPv6 = true;
     # wireless.enable = true;
     nameservers = [
-      "1.1.1.1#cloudflare-dns.com"
-      "9.9.9.9#dns.quad9.net"
+      "1.1.1.1" # Cloudflare
+      "9.9.9.9" # Quad9
     ];
   };
 }
