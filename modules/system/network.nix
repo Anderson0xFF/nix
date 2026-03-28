844114eaf64e6badae07d1a33fc7a805568ccd75
Anderson Almeida <Anderson Almeida>
2026-03-28 20:53:42 -0300

    adiciona configuração do resolv.conf e desabilita resolvconf

--- modules/system/network.nix
+++ modules/system/network.nix
@@ -13,5 +13,12 @@
       "1.1.1.1" # Cloudflare
       "9.9.9.9" # Quad9
     ];
+    resolvconf.enable = false;
   };
+
+  environment.etc."resolv.conf".text = ''
+    nameserver 1.1.1.1
+    nameserver 9.9.9.9
+    options edns0
+  '';
 }
