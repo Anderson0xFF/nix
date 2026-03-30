81bbece9fa5766f9292c9fdba5b243cd03314a8c
Anderson Almeida <andersonvsc1998@gmail.com>
2026-03-29 21:15:36 -0300

    reabilita navegação com setas no Neovim

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -42,6 +42,18 @@ require("nvchad.configs.lspconfig").defaults()
 
 local servers = { "html", "cssls", "rust_analyzer", "nil_ls", "ts_ls" }
 vim.lsp.enable(servers)
+
+-- Reabilitar navegação com setas (NvChad desabilita por padrão)
+local nomap = vim.keymap.del
+
+pcall(function()
+  for _, mode in ipairs({ "n", "i", "v" }) do
+    nomap(mode, "<Up>")
+    nomap(mode, "<Down>")
+    nomap(mode, "<Left>")
+    nomap(mode, "<Right>")
+  end
+end)
     '';
   };
 }
