24c4dac3c59a56dde64e727828d862ba75eb2b3b
Anderson Almeida <andersonvsc1998@gmail.com>
2026-04-01 22:07:36 -0300

    atualiza configurações do ghostty para ajustar fonte e tamanho

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -4,11 +4,12 @@
     enable = true;
     package = pkgs.ghostty;
     settings = {
-      font-family = "JetBrainsMono Nerd Font";
+      #font-family = "JetBrainsMono Nerd Font";
+      font-family = "Maple Mono";
       #theme = "Catppuccin Mocha";
       background = "#000000"; # Transparência com alpha (hex + cc para 80% de opacidade)
       # background-opacity = 0.8;
-      font-size = 12.5;
+      font-size = 14;
       window-decoration = false;
       confirm-close-surface = false;
     };
