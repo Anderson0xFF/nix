6b6fb3eab87bfc5527bb8d96544ced6f37e97842
Anderson Almeida <andersonvsc1998@gmail.com>
2026-04-04 20:17:54 -0300

    adiciona extraPackages para expor o manifesto de layer Vulkan do RenderDoc

--- modules/system/graphics.nix
+++ modules/system/graphics.nix
@@ -5,6 +5,9 @@
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    # Expõe o manifesto de layer Vulkan do RenderDoc via VK_ADD_LAYER_PATH
+    # (evita a mensagem "layer not registered" e a tentativa de escrever em /etc).
+    extraPackages = [ pkgs.renderdoc ];
   };
 
   environment.systemPackages = with pkgs; [
