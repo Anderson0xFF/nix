6773a6b20636fa434981a2955eec67ef3d09d82c
Anderson Almeida <andersonvsc1998@gmail.com>
2026-04-11 13:21:40 -0300

    adiciona plugin zsh-autocomplete ao shell.nix

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -4,6 +4,15 @@
     enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
+
+    plugins = [
+      {
+        name = "zsh-autocomplete";
+        src = "${pkgs.zsh-autocomplete}/share/zsh-autocomplete";
+        file = "zsh-autocomplete.plugin.zsh";
+      }
+    ];
+
     shellAliases = {
       explorer = "yazi";
     };
